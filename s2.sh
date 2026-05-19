53ef2791313c9b30dc5a3dc2b0f06808fc931b86
Bluepost855 <elpadillaga@uide.edu.ec>
2026-05-19 01:27:00 +0000

    wip> first commands and C code

---
+++ s2.sh
@@ -0,0 +1,20 @@
+#
+man gcc
+#
+man --help
+#
+gcc hola.c -o hola_binario
+#
+file hola_binario
+#
+file hola.c
+# Este comando sirve para imprimir el directorio actual, equivalente a saber el directorio en windows
+pwd
+#Este comando me lleva al directiorio 'home' de mi usuario
+cd
+# ruta absoluta es la ruta completa desde el directorio raiz \
+
+#ruta relativa no tiene toda la ruta completa 
+
+# lista archivos y directorios
+ls
