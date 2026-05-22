54c35e9694c0d7c14cd4344d006f530e035b698d
Bluepost855 <elpadillaga@uide.edu.ec>
2026-05-22 01:51:45 +0000

    práctica nuevos comandos

---
+++ s3.sh
@@ -0,0 +1,19 @@
+#
+pwd
+#
+ls
+# make directory
+mkdir
+# change directory: cambiar directorio
+cd
+# "ls --all" o "ls -a" muestra todos los archivos y directotios ocultos (los que empiezan con puntos) y generales
+ls --all
+# permite moverse al directorio que contiene al directorio actual
+cd ..
+# mi directorio actual es el directorio .
+# ls --help muestra las entradas alfabeticas o comandos para diferentes usos
+ls --help
+# para crear archivos touch + "nombre_del _archivo" sin comillas y siempre separado con guión bajo
+touch nuevo_archivo
+# ruta absoluta
+
