c3adcbcce0421435d77be23139b9c21c39061de8
Bluepost855 <elpadillaga@uide.edu.ec>
2026-05-25 00:10:05 +0000

    wip: primeros comandos y codigo en C

--- s2.sh
+++ s2.sh
@@ -1,21 +1,19 @@
-#
+# manual de instrucciones del gcc, y verifica que esté instalado si se muestra el manual
 man gcc
-#
+# Comando para obtener un manual de ayuda simplificado
 man --help
-#
+# Ese comando es para compilar el codigo fuente "hola.c"
 gcc hola.c -o hola_binario
-#
+# Que tipo de archivo es mi binario
 file hola_binario
-#
+# Muestra un archivo de código fuente en lenguaje C
 file hola.c
 # Este comando sirve para imprimir el directorio actual, equivalente a saber el directorio en windows
 pwd
 #Este comando me lleva al directiorio 'home' de mi usuario
 cd
 # ruta absoluta es la ruta completa desde el directorio raiz \
-
 #ruta relativa no tiene toda la ruta completa 
-
 # lista archivos y directorios en el directorio actual
 ls
 # 
