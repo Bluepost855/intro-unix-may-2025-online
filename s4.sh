661f308379be99386ffceab07690d26e90580982
Bluepost855 <elpadillaga@uide.edu.ec>
2026-05-29 01:52:03 +0000

    wip: final commit s4

---
+++ s4.sh
@@ -0,0 +1,3 @@
+#
+ls -la
+# r=4 ; w=2 ; x=1 | read, write, execution
---
+++ s5.sh
@@ -0,0 +1,32 @@
+# ver todo el contenido del directorio
+ls -la
+# Crear un nuevo archivo
+touch mi_archivo
+#
+chmod 777 mi_archivo
+# 
+id
+#
+sudo su
+#
+id
+# Salir de usuario root
+exit
+#
+chmod 457 mi_archivo
+# cambiiar los permisos usando letras. u=user ; g=group ; o=others ; a=all
+chmod ugoa+rwx mi_archivo_2
+#
+ls -la
+#
+umask
+# me lleva al directorio home
+cd
+#
+stat NombreDelArchivo
+#
+ls -li
+#
+cd /
+#
+cd usr/
