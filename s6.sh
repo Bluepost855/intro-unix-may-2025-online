c6cc1fe48f14b18404e336fdf875708657380dd6
Bluepost855 <elpadillaga@uide.edu.ec>
2026-06-05 01:59:30 +0000

    Week 5: installing libraries

---
+++ s6.sh
@@ -0,0 +1,10 @@
+# antes de instalar cualquier librería se debe hacer un "sudo apt update", (esto es para super usuaro o root), apt
+# (es un gestor de paquetes)
+# se acualiza las librerias para evitar el dependency hell. si usamos versiones qeu no reciben soporte 
+# es muy rpblable que exista una puerta tracera
+sudo apt update
+Sudo apt upgrade
+suma.py
+calculadora.py
+
+sudo apt install nmap
